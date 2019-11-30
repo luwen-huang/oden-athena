e2b27f7df5780a31f398ee7510f18f7a28dfa020
luwen-huang <huang.luwen@gmail.com>
2019-11-30 11:39:04 -0500

    build

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,22 @@
 
 const nav = document.querySelector('#toc');
 const navTop = nav.offsetTop;
+
+
+$(document).ready(  function() {
+
+  // class name of a brick item
+  var brickItemSelector = `.oden-athena__brick`
+
+  var brickItemWidth = $(brickItemSelector).outerWidth()
+  // console.log('brickItemWidth:', brickItemWidth);
+
+  $('.oden-athena__masonry-grid').masonry({
+    // options
+    itemSelector: brickItemSelector,
+    columnWidth: 1
+  });
+
+  console.log('masonry-grid applied');
+
+});
